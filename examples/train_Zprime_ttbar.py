9041c0f44222b378c7a46d305851e433095dc08f
David Olivier Jamin <david.jamin@cern.ch>
2018-02-07 13:18:47 +0100

    modify training variables, cut and parameters for Zprime_tt and add RSGraviton_ww train

--- examples/train_Zprime_ttbar.py
+++ examples/train_Zprime_ttbar.py
@@ -4,29 +4,37 @@ from bin.tools import train
 #### input variables to be used for training ###
 variables = [
 
-    'Jet1_trk02_tau1',
-    'Jet1_trk02_tau2',
-    'Jet1_trk02_tau3',
     'Jet1_trk02_tau32',
-    'Jet1_trk02_tau21',
     'Jet1_trk02_tau31',
+    'Jet1_trk02_tau21',
     'Jet1_trk02_SD_Corr_m',
-    'Jet1_trk02_Corr_MetCorr_m',
+    'Jet1_trk02_SD_Corr_pt',
+    'Jet2_trk02_tau32',
+    'Jet2_trk02_tau31',
+    'Jet2_trk02_tau21',
+    'Jet2_trk02_SD_Corr_m',
+    'Jet2_trk02_SD_Corr_pt',
+    'missingET',
+    'rapiditySeparation_trk02',
+    'transverseMomentumAsymmetry_trk02',
 ]
 
 #### define signal and background trees + selection to be applied ###
 
-eospath = '/eos/experiment/fcc/hh/analyses/Zprime_tt/heppy_outputs/fcc_v02/'
+eospath = '/eos/experiment/fcc/hh/analyses/Zprime_tt/heppy_outputs/fcc_v02_mvaQCD/'
 
-bkgTree = eospath + 'pp_jj_lo/heppy.FCChhAnalyses.Zprime_tt.TreeProducer.TreeProducer_1/tree.root'
-sigTree = eospath + 'pp_Zprime_20TeV_ttbar/heppy.FCChhAnalyses.Zprime_tt.TreeProducer.TreeProducer_1/tree.root'
+bkgTree = eospath + 'pp_jj_lo_filter_pTjet7_5TeV/heppy.FCChhAnalyses.Zprime_tt.TreeProducer.TreeProducer_1/tree.root'
+sigTree = eospath + 'pp_Zprime_20TeV_ttbar_qcdBDTtrain/heppy.FCChhAnalyses.Zprime_tt.TreeProducer.TreeProducer_1/tree.root'
 
-cuts    = 'Jet1_trk02_Corr_MetCorr_pt > 7500. && Jet1_trk02_Corr_MetCorr_pt < 12500. && abs(Jet1_trk02_Corr_MetCorr_eta) < 2.5'
+SIGcuts = ''
+BKGcuts = 'Mj1j2_trk02>10000.'
+cuts    = [SIGcuts,BKGcuts]
 
 #### define MVA method ###
 
 method = 'BDT'
-nTrain = 5000
+#nTrain = 50000
+nTrain = 0
 label = 'Zp_M_20TeV'
 
 
@@ -36,6 +44,6 @@ train(bkgTree, sigTree, variables, method, nTrain, label, cuts)
 
 #### check the output with the TMVAGUI ####
 
-outFileName = 'TMVA_{}_{}.root'.format(method, label)
-ROOT.TMVA.TMVAGui(outFileName, label)
-raw_input('Press Enter to continue...')
+#outFileName = 'TMVA_{}_{}.root'.format(method, label)
+#ROOT.TMVA.TMVAGui(outFileName, label)
+#raw_input('Press Enter to continue...')
